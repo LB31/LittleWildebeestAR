7be8fefa1441b635a16c0fb40b89a48bd02e9279
Leonid <leonidb93@gmail.com>
2019-04-29 01:52:33 +0200

    set up the project, add vuforia and first marker, implement basic TrackableEventHandler script

---
+++ Assets/_Scripts/TrackableEventHandlerFrontPage.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class TrackableEventHandlerFrontPage : DefaultTrackableEventHandler
+{
+
+
+    protected override void Start() {
+        base.Start();
+    }
+
+    protected override void OnDestroy() {
+        base.OnDestroy();
+    }
+
+ 
+
+    protected override void OnTrackingFound() {
+        base.OnTrackingFound();
+
+        // ToDo do more stuff
+        print("yay");
+        
+    }
+
+    protected override void OnTrackingLost() {
+        base.OnTrackingLost();
+
+        // ToDo do more stuff
+        Debug.Log("man");
+        
+    }
+}
