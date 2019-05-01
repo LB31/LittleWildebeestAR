e693ad28222aa12249218af7a4bc8f7a26740522
Leonid <leonidb93@gmail.com>
2019-05-01 11:02:23 +0200

    Implement database for animals

---
+++ Assets/_Scripts/JsonEditorButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(DatabaseController))]
+public class JsonEditorButton : Editor
+{
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+
+        DatabaseController dc = (DatabaseController)target;
+        if (GUILayout.Button("Save data")) {
+            dc.SerializeData();
+        }
+    }
+}
