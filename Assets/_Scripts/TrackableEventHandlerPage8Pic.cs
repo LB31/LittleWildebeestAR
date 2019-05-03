9174472b1037978d5e299089063a1724e1ea858b
Leonid <leonidb93@gmail.com>
2019-05-03 11:23:51 +0200

    add pig marker

---
+++ Assets/_Scripts/TrackableEventHandlerPage8Pic.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class TrackableEventHandlerPage8Pic : TrackableEventHandlerParent
+{
+    public AudioSource soundTarget;
+    public AudioClip clipTarget;
+    private AudioSource[] allAudioSources;
+
+    protected override void Start() {
+        base.Start();
+    }
+
+    protected override void OnDestroy() {
+        base.OnDestroy();
+    }
+
+ 
+
+    protected override void OnTrackingFound() {
+        base.OnTrackingFound();
+
+
+        soundTarget.clip = clipTarget;
+        soundTarget.loop = false;
+        soundTarget.Play();
+
+    }
+
+    protected override void OnTrackingLost() {
+        base.OnTrackingLost();
+
+        soundTarget.Stop();
+
+    }
+}
